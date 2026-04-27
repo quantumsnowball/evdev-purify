9b2353f50a7c731703d9f2a286776a06336ffbd3
Quantum Snowball <quantum.snowball@gmail.com>
2026-04-27 20:15:44 +0800

    feat: basic feature of a button remapper

--- evdev_purify/ffb_wheel/purifier.py
+++ evdev_purify/ffb_wheel/purifier.py
@@ -1,11 +1,43 @@
 import logging
 
 from evdev import UInput
+from evdev import ecodes as ec
 from evdev.ecodes import EV_KEY
 
 from evdev_purify.purifier import Purifier as Base
 
 logger = logging.getLogger(__file__)
+
+KEYMAPS = {
+    # L1
+    292: ec.KEY_Z,
+    # R1
+    293: ec.KEY_X,
+
+    # X
+    290: None,
+    # Y
+    291: None,
+    # A
+    288: ec.KEY_ENTER,
+    # B
+    289: ec.KEY_ESC,
+
+    # L3
+    298: None,
+    # R3
+    299: None,
+
+    # Task
+    296: ec.KEY_C,
+    # Function
+    301: ec.KEY_L,
+    # Menu
+    297: None,
+
+    # Home
+    300: ec.KEY_H,
+}
 
 
 class Purifier(Base):
@@ -28,5 +60,9 @@ class Purifier(Base):
 
             # only interested in single event key-press package
             if p.types == {EV_KEY, }:
-                # TODO: you can then safely remap your key into any keyboard code and use it in your game
-                logger.info(p)
+                # check new code from map
+                if (new_code := KEYMAPS[p[0].code]) is not None:
+                    # replace if new code is defined
+                    p[0].code = new_code
+                    # then send the code to new device
+                    p.send(self._dst_dev)
