062ee75cf70978d87bcbc8c0e3dba0001dfb67f8
Quantum Snowball <quantum.snowball@gmail.com>
2026-04-27 16:11:44 +0800

    feat: ffb-wheel purifier defines its own dst_dev as a keyboard UInput device

--- evdev_purify/ffb_wheel/purifier.py
+++ evdev_purify/ffb_wheel/purifier.py
@@ -1,5 +1,7 @@
 import logging
 
+from evdev import UInput
+
 from evdev_purify.purifier import Purifier as Base
 
 logger = logging.getLogger(__file__)
@@ -8,6 +10,11 @@ logger = logging.getLogger(__file__)
 class Purifier(Base):
     def __init__(self, name: str) -> None:
         super().__init__(name)
+        self._dst_dev = UInput(name=f'Purifier: {name}')
 
     def run(self) -> None:
         logger.info(f'Starting Purifier on {self._src_dev_path} ...')
+
+        # intercept all src events and process them
+        for p in self._packages:
+            logger.info(p)
