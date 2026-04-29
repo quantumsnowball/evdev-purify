630319e524625be90e5a29b87fe123055e0e8dba
Quantum Snowball <quantum.snowball@gmail.com>
2026-04-29 20:30:50 +0800

    proj: ffb-wheel is now rebranded to a new subcommand

---
+++ evdev_purify/ffb_wheel/main.py
@@ -0,0 +1,29 @@
+import logging
+from typing import Annotated
+
+from typer import Argument, Option, Typer
+
+app = Typer()
+
+logger = logging.getLogger(__file__)
+
+
+@app.command(
+    no_args_is_help=True,
+    help='ffb-wheel event purifier',
+)
+def ffb_wheel(
+    name: Annotated[str, Argument(help='The device name from evtest')],
+    debug: Annotated[bool, Option(help='Enable debug mode verbose output')] = False,
+) -> None:
+    # logger
+    logging.basicConfig(
+        level=logging.DEBUG if debug else logging.INFO,
+        format='%(levelname)s [%(filename)s:%(lineno)d] %(message)s' if debug else '%(message)s',
+    )
+
+    # device
+
+    # run
+
+    logger.info(f'Should grab the device {name=} and purifier it or even remap its events')
