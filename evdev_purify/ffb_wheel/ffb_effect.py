9aaec7dfa26f5a224c9ec6d7d61212d269bccbbe
Quantum Snowball <quantum.snowball@gmail.com>
2026-04-30 05:50:53 +0800

    fix: here should have retry loop as well

--- evdev_purify/ffb_wheel/ffb_effect.py
+++ evdev_purify/ffb_wheel/ffb_effect.py
@@ -32,34 +32,36 @@ class FFBEffectManager:
 
     def _worker(self) -> None:
         logger.info('ffb effect manager starting monitoring game effect events ...')
-        try:
-            for event in self._dst_dev.read_loop():
-                # Handle the special uinput events
-                if event.type == EV_UINPUT:
-                    # ff upload
-                    if event.code == UI_FF_UPLOAD:
-                        upload = self._dst_dev.begin_upload(event.value)
+        # retry loop
+        while True:
+            try:
+                for event in self._dst_dev.read_loop():
+                    # Handle the special uinput events
+                    if event.type == EV_UINPUT:
+                        # ff upload
+                        if event.code == UI_FF_UPLOAD:
+                            upload = self._dst_dev.begin_upload(event.value)
 
-                        # Checks if this is a new effect
-                        if upload.effect.id not in self._effects:
-                            self._effects.add(upload.effect.id)
-                            # Setting id to 1 indicates that a new effect must be allocated
-                            upload.effect.id = -1
+                            # Checks if this is a new effect
+                            if upload.effect.id not in self._effects:
+                                self._effects.add(upload.effect.id)
+                                # Setting id to 1 indicates that a new effect must be allocated
+                                upload.effect.id = -1
 
-                        self._src_dev.upload_effect(upload.effect)
-                        upload.retval = 0
-                        self._dst_dev.end_upload(upload)
-                    # ff erase
-                    elif event.code == UI_FF_ERASE:
-                        erase = self._dst_dev.begin_erase(event.value)
-                        erase.retval = 0
-                        self._src_dev.erase_effect(erase.effect_id)
-                        self._effects.remove(erase.effect_id)
-                        self._dst_dev.end_erase(erase)
-                # Forward writes to actual rumble device.
-                elif event.type == EV_FF:
-                    self._src_dev.write(event.type, event.code, event.value)
-        except Exception:
-            logger.info('dst_dev closed, breaking loop ...')
-        finally:
-            logger.info('ffb effect manager has stopped, exiting ...')
+                            self._src_dev.upload_effect(upload.effect)
+                            upload.retval = 0
+                            self._dst_dev.end_upload(upload)
+                        # ff erase
+                        elif event.code == UI_FF_ERASE:
+                            erase = self._dst_dev.begin_erase(event.value)
+                            erase.retval = 0
+                            self._src_dev.erase_effect(erase.effect_id)
+                            self._effects.remove(erase.effect_id)
+                            self._dst_dev.end_erase(erase)
+                    # Forward writes to actual rumble device.
+                    elif event.type == EV_FF:
+                        self._src_dev.write(event.type, event.code, event.value)
+            except OSError:
+                logger.info('Device disconnected, retrying ...')
+            except Exception as e:
+                logger.error(e)
