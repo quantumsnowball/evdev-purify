d201662401b2752d6921e45a8fa055132e541413
Quantum Snowball <quantum.snowball@gmail.com>
2026-04-30 02:15:00 +0800

    feat: apply all logic from the working demo script

--- evdev_purify/ffb_wheel/ffb_effect.py
+++ evdev_purify/ffb_wheel/ffb_effect.py
@@ -2,6 +2,7 @@ import logging
 import threading
 
 from evdev import InputDevice, UInput
+from evdev.ecodes import EV_FF, EV_UINPUT, UI_FF_ERASE, UI_FF_UPLOAD
 
 logger = logging.getLogger(__file__)
 
@@ -14,6 +15,7 @@ class FFBEffectManager:
     ) -> None:
         self._src_dev = src_dev
         self._dst_dev = dst_dev
+        self._effects = set[int]()
         self._thread = threading.Thread(target=self._worker, daemon=True)
         self._thread.start()
 
@@ -21,9 +23,31 @@ class FFBEffectManager:
         logger.info('ffb effect manager starting monitoring game effect events ...')
         try:
             for event in self._dst_dev.read_loop():
+                # Handle the special uinput events
+                if event.type == EV_UINPUT:
+                    # ff upload
+                    if event.code == UI_FF_UPLOAD:
+                        upload = self._dst_dev.begin_upload(event.value)
 
-                # do all ffb related event and passthrough to src_dev
-                pass
+                        # Checks if this is a new effect
+                        if upload.effect.id not in self._effects:
+                            self._effects.add(upload.effect.id)
+                            # Setting id to 1 indicates that a new effect must be allocated
+                            upload.effect.id = -1
+
+                        self._src_dev.upload_effect(upload.effect)
+                        upload.retval = 0
+                        self._dst_dev.end_upload(upload)
+                    # ff erase
+                    elif event.code == UI_FF_ERASE:
+                        erase = self._dst_dev.begin_erase(event.value)
+                        erase.retval = 0
+                        self._src_dev.erase_effect(erase.effect_id)
+                        self._effects.remove(erase.effect_id)
+                        self._dst_dev.end_erase(erase)
+                # Forward writes to actual rumble device.
+                elif event.type == EV_FF:
+                    self._src_dev.write(event.type, event.code, event.value)
         except Exception:
             logger.info('dst_dev closed, breaking loop ...')
         finally:
