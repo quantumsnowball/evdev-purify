9997af403a125ba80f75335e4af1d8f36c8f73bd
Quantum Snowball <quantum.snowball@gmail.com>
2026-05-01 00:27:04 +0800

    RealDevice should be context manager, naturally break the packages iterator will exit the context manager

--- evdev_purify/real_device.py
+++ evdev_purify/real_device.py
@@ -1,7 +1,10 @@
 import logging
-from typing import Any, override
+from typing import Any, Iterator, Self, override
 
 from evdev import InputDevice
+from evdev.ecodes import EV_SYN
+
+from .package import Event, Package
 
 logger = logging.getLogger(__file__)
 
@@ -14,3 +17,24 @@ class RealDevice(InputDevice):
 
     def __str__(self):
         return f"RealDevice('{self.device.path}', name='{self.device.name}')"
+
+    def __enter__(self) -> Self:
+        return self
+
+    def __exit__(self, *_) -> None:
+        pass
+
+    @property
+    def packages(self) -> Iterator[Package]:
+        try:
+            package = Package()
+            for e in self.read_loop():
+                # append as custom Event type
+                package.append(Event(e))
+                if e.type == EV_SYN:
+                    yield package
+                    package = Package()
+        except OSError:
+            logger.info(f'Disconnected: {str(self)}')
+        except Exception as e:
+            logger.error(e)
