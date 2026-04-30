a30bc94aa5904d3481fce4ee828a2f97ffd85975
Quantum Snowball <quantum.snowball@gmail.com>
2026-05-01 01:13:10 +0800

    find or wait for a real device connected

--- evdev_purify/real_device.py
+++ evdev_purify/real_device.py
@@ -1,6 +1,7 @@
 import logging
-from typing import Any, Iterator, Self, override
+from typing import Any, Callable, Iterator, Self, override
 
+import pyudev
 from evdev import InputDevice
 from evdev.ecodes import EV_SYN
 
@@ -38,3 +39,37 @@ class RealDevice(InputDevice):
             logger.info(f'Disconnected: {str(self)}')
         except Exception as e:
             logger.error(e)
+
+    @classmethod
+    def find_or_wait_for(
+        cls,
+        name: str,
+        is_targeted_device: Callable[[Self], bool]
+    ) -> Self:
+        context = pyudev.Context()
+        monitor = pyudev.Monitor.from_netlink(context)
+        monitor.filter_by(subsystem='input')
+
+        # retry loop for device connection
+        while True:
+            # try to find and return the device if it already connected
+            for item in context.list_devices(subsystem='input'):
+                try:
+                    if item.device_node is not None:
+                        dev = cls(item.device_node)
+                        if is_targeted_device(dev):
+                            logger.info(f'Found existing device: {name} at {item.device_node}')
+                            return dev
+                except Exception:
+                    continue
+            # then block until any device is added, and check if this is the targeted device
+            logger.info(f'Waiting for device: {name}')
+            for item in iter(monitor.poll, None):
+                try:
+                    if item.device_node is not None and item.action == 'add':
+                        dev = cls(item.device_node)
+                        if is_targeted_device(dev):
+                            logger.info(f'Found newly added device: {name} at {item.device_node}')
+                            return dev
+                except Exception:
+                    continue
