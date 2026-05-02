62cfc790c8bb913fb7468fd36eb1a1f9a20bca16
Quantum Snowball <quantum.snowball@gmail.com>
2026-05-02 22:27:56 +0800

    feat: assigned all ordinary keymaps

--- evdev_purify/ffb_wheel_companion/keymaps.py
+++ evdev_purify/ffb_wheel_companion/keymaps.py
@@ -22,33 +22,33 @@ BindingTargets = tuple[BindingTarget, BindingTarget, BindingTarget]
 
 BINDINGS: dict[BindingSource, BindingTargets] = {
     # L1
-    292: (ec.KEY_Q, None, None),
+    292: (ec.KEY_Q, ec.KEY_A, ec.KEY_D),
     # R1
-    293: (ec.KEY_E, None, None),
+    293: (ec.KEY_E, ec.KEY_W, ec.KEY_S),
 
     # X
-    290: (ec.KEY_F, ec.KEY_5, None),
+    290: (ec.KEY_F, ec.KEY_R, ec.KEY_COMMA),
     # Y
-    291: (ec.KEY_G, ec.KEY_6, None),
+    291: (ec.KEY_G, ec.KEY_T, ec.KEY_DOT),
     # A
-    288: (ec.KEY_ENTER, None, None),
+    288: (ec.KEY_ENTER, ec.KEY_Y, ec.KEY_I),
     # B
-    289: (ec.KEY_ESC, None, None),
+    289: (ec.KEY_ESC, ec.KEY_U, ec.KEY_O),
 
     # L3
-    298: (ec.KEY_Z, None, None),
+    298: (ec.KEY_Z, ec.KEY_1, ec.KEY_2),
     # R3
-    299: (ec.KEY_X, None, None),
+    299: (ec.KEY_X, ec.KEY_3, ec.KEY_4),
 
     # Task
-    296: (ec.KEY_C, None, None),
-    # Function
-    301: (ec.KEY_V, None, None),
+    296: (ec.KEY_C, ec.KEY_5, ec.KEY_6),
     # Menu
-    297: (ec.KEY_B, None, None),
+    297: (ec.KEY_B, ec.KEY_7, ec.KEY_8),
+    # Function
+    301: (ec.KEY_V, ec.KEY_9, ec.KEY_0),
 
     # Home
-    300: (ec.KEY_H, None, None),
+    300: (ec.KEY_H, ec.KEY_SPACE, ec.KEY_SPACE),
 }
 
 
