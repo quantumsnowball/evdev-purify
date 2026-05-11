303f209dea009026f01f354e4f28f15b2f643574
Quantum Snowball <quantum.snowball@gmail.com>
2026-05-12 00:59:30 +0800

    modify the value, but this doesn't work

--- evdev_purify/xbox_wheel/purifier.py
+++ evdev_purify/xbox_wheel/purifier.py
@@ -1,7 +1,7 @@
 import logging
 
-from evdev import InputDevice
-from evdev.ecodes import EV_KEY, EV_MSC
+from evdev import AbsInfo, InputDevice
+from evdev.ecodes import ABS_X, EV_ABS, EV_FF, EV_KEY, EV_MSC, EV_SYN
 
 from evdev_purify.purifier import Purifier as Base
 from evdev_purify.real_device import RealDevice
@@ -34,16 +34,29 @@ class Purifier(Base):
     def run(self) -> None:
         with (
             RealDevice.find_or_wait_for(self._name, self._is_target, grab=True) as real_dev,
-            VirtualDevice.from_device(real_dev, name=f'Pure: {self._name}') as virtual_dev,
+            # VirtualDevice.from_device(real_dev, name=f'Pure: {self._name}') as virtual_dev,
         ):
-            # then process all src events
-            for package in real_dev.packages(drop=(EV_MSC, )):
-                # if a package contains more than one EV_KEY event, consider these noise
-                if package.count(EV_KEY) > 1:
-                    # only log very high event count package for debug purpose
-                    if package.items_count >= self._log_threshold:
-                        logger.info(f'BIG: {package}')
-                    # skip to next
-                    continue
-                # passthrough all other irrelevant events
-                virtual_dev.send(package)
+            virtual_caps = dict(real_dev.capabilities())
+            virtual_caps[EV_ABS] = [
+                (ABS_X, AbsInfo(128, 0, 255, 0, 15, 0)) if itm[0] == ABS_X else itm
+                for itm in virtual_caps[EV_ABS]
+            ]
+            for ev in (EV_SYN, EV_FF):
+                if ev in virtual_caps:
+                    del virtual_caps[ev]
+            with VirtualDevice(events=virtual_caps, name=f'Pure: {self._name}') as virtual_dev:
+                # then process all src events
+                for package in real_dev.packages(drop=(EV_MSC, )):
+                    # scale the wheel into 0 to 255 low resolutio thumb stick events
+                    for e in package:
+                        if e.type == EV_ABS and e.code == ABS_X:
+                            e.value = (e.value + 32768) // 256
+                    # if a package contains more than one EV_KEY event, consider these noise
+                    if package.count(EV_KEY) > 1:
+                        # only log very high event count package for debug purpose
+                        if package.items_count >= self._log_threshold:
+                            logger.info(f'BIG: {package}')
+                        # skip to next
+                        continue
+                    # passthrough all other irrelevant events
+                    virtual_dev.send(package)
