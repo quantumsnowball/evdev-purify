563426713904c534387890f4ba1d8a54f3f09aea
Quantum Snowball <quantum.snowball@gmail.com>
2026-08-13 03:49:39 +0800

    idea: a de-noise counter, min count to enter a layer

--- evdev_purify/ffb_wheel_companion/layer.py
+++ evdev_purify/ffb_wheel_companion/layer.py
@@ -23,6 +23,8 @@ class LayerManager:
         }
         # state
         self._layer = Layer.BASE
+        self._layer_left_event_count = 0
+        self._layer_right_event_count = 0
 
     def __enter__(self) -> Self:
         self._layer = Layer.BASE
@@ -46,18 +48,37 @@ class LayerManager:
     def decide_layer(self, package: Package, *, threshold: float) -> None:
         # choose which layer based on the first event
         e = package[0]
-        new_layer = (
-            # non axis signal, stay on the same level
-            self._layer if e.type != EV_ABS else
-            # axis signal except L2 or R2 axis signal, stay on the same level
-            self._layer if e.code not in (ABS_RX, ABS_RY) else
-            # L2 and larger than threshold, switch to left layer
-            Layer.LEFT if e.code == ABS_RX and e.value >= threshold else
-            # R2 and larger than threshold, switch to right layer
-            Layer.RIGHT if e.code == ABS_RY and e.value >= threshold else
-            # otherwise
-            Layer.BASE
-        )
+        # new_layer = (
+        #     # non axis signal, stay on the same level
+        #     self._layer if e.type != EV_ABS else
+        #     # axis signal except L2 or R2 axis signal, stay on the same level
+        #     self._layer if e.code not in (ABS_RX, ABS_RY) else
+        #     # L2 and larger than threshold, switch to left layer
+        #     Layer.LEFT if e.code == ABS_RX and e.value >= threshold else
+        #     # R2 and larger than threshold, switch to right layer
+        #     Layer.RIGHT if e.code == ABS_RY and e.value >= threshold else
+        #     # otherwise
+        #     Layer.BASE
+        # )
+
+        # change state and choose a layer
+        if e.type != EV_ABS:
+            new_layer = self._layer
+        elif e.code not in (ABS_RX, ABS_RY):
+            new_layer = self._layer
+        elif e.code == ABS_RX and e.value >= threshold:
+            self._layer_left_event_count += 1
+            new_layer = Layer.LEFT if self._layer_left_event_count >= 5 else Layer.BASE
+            self._layer_right_event_count = 0
+        elif e.code == ABS_RY and e.value >= threshold:
+            self._layer_right_event_count += 1
+            new_layer = Layer.RIGHT if self._layer_right_event_count >= 5 else Layer.BASE
+            self._layer_left_event_count = 0
+        else:
+            new_layer = Layer.BASE
+            self._layer_left_event_count = 0
+            self._layer_right_event_count = 0
+
         # if layer changed, clear previous layer
         if new_layer != self._layer:
             self.clear_layer(self._layer)
